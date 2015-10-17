6de86c3429b6a2a587e29ab853255dedcc92b691
Ivan Stoilov <stoilov.ivan@gmail.com>
2015-10-17 22:36:00 +0000

    fix for sensor temperature

--- jobs/classes/sensor-temperature.php
+++ jobs/classes/sensor-temperature.php
@@ -4,7 +4,9 @@ class SensorTemperature {
   public static function getCurrentTemperature() {
     $data = file_get_contents('/sys/bus/w1/devices/28-01158278daff/w1_slave');
 
-    list($rest, $temp) = explode('=', $data);
+    list($rest, $temp) = explode('t=', $data);
+
+    $temp = intval($temp) / 1000;
 
     return $temp;
   }
