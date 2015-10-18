d0cf2cc7bb49066660544a994609caecb07c8181
Ivan Stoilov <stoilov.ivan@gmail.com>
2015-10-18 11:02:51 +0200

    Log file added

---
+++ jobs/classes/config.php
@@ -0,0 +1,5 @@
+<?php
+
+class Config {
+  public static $logFile = '/home/pi/Tempri/logs/tempri.log';
+}
--- jobs/classes/log.php
+++ jobs/classes/log.php
@@ -1,14 +1,14 @@
 <?php
 
-class Log {
-  private static $file = '../logs/tempri.log';
+require "./config.php";
 
+class Log {
   public static function write($message) {
     $date = date('[Y-m-d H:i:s] ');
     var $log = $date . $message . "\n";
 
     echo $log;
 
-    file_put_contents(self::$file, $log, FILE_APPEND);
+    file_put_contents(Config::$logFile, $log, FILE_APPEND);
   }
 }
