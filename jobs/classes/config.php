0c78234334c799d41eb295b2118125518f644ad7
Ivan Stoilov <stoilov.ivan@gmail.com>
2015-10-18 16:33:53 +0000

    adjust servo on/off values better

--- jobs/classes/config.php
+++ jobs/classes/config.php
@@ -2,6 +2,6 @@
 
 class Config {
   const LOG_FILE = '/home/pi/Tempri/logs/tempri.log';
-  const SERVO_ON_FREQ = 2000;
-  const SERVO_OFF_FREQ = 1000;
+  const SERVO_ON_FREQ = 1800;
+  const SERVO_OFF_FREQ = 600;
 }
--- jobs/servo.py
+++ jobs/servo.py
@@ -9,10 +9,10 @@ servo = PWM.Servo()
 freq = int(float(sys.argv[1]));
 print freq;
 
-if (freq >= 1000 and freq <= 2000):
+if (freq >= 100 and freq <= 2000):
         servo.set_servo(17, freq);
 else:
         print "Value not in the 1000-2000 range!"
 
-time.sleep(0.3)
+time.sleep(0.5)
 servo.stop_servo(17)
