845a564feef77663e124536e9cab8b8486afc01e
Ivan Stoilov <stoilov.ivan@gmail.com>
2015-10-19 22:06:39 +0200

    new values for servo on/off

--- jobs/classes/config.php
+++ jobs/classes/config.php
@@ -2,6 +2,6 @@
 
 class Config {
   const LOG_FILE = '/home/pi/Tempri/logs/tempri.log';
-  const SERVO_ON_FREQ = 1800;
-  const SERVO_OFF_FREQ = 600;
+  const SERVO_ON_FREQ = 1200;
+  const SERVO_OFF_FREQ = 2000;
 }
